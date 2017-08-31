aa1474ca5c0411d3e8d4d074a4dd7ec745ce4601
mushoodEsokia <my@esokia.com>
2017-08-31 17:45:45 +0400

    Form with date

--- src/AppBundle/Controller/NewsController.php
+++ src/AppBundle/Controller/NewsController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class NewsController extends Controller
 {
@@ -27,10 +28,13 @@ class NewsController extends Controller
         $defiMedias = $repositoryNews->getNewsByCategoryAndNewspaper(1,2);
         $mauriciens = $repositoryNews->getNewsByCategoryAndNewspaper(1,3);
 
+        $date = new \DateTime();
+
         return $this->render('AppBundle:news:index.html.twig', array(
           'expresses' => $expresses,
           'defiMedias' => $defiMedias,
           'mauriciens' => $mauriciens,
+          'date' => $date->format('Y-m-d')
         ));
     }
 
@@ -40,26 +44,47 @@ class NewsController extends Controller
     public function dateAction(Request $request)
     {
         $repositoryNews = $this->container->get('doctrine')
-          ->getManager()
-          ->getRepository('AppBundle:News')
+            ->getManager()
+            ->getRepository('AppBundle:News')
         ;
 
         $repositoryNewspaper = $this->container->get('doctrine')
-          ->getManager()
-          ->getRepository('AppBundle:Newspaper')
+            ->getManager()
+            ->getRepository('AppBundle:Newspaper')
         ;
 
-        $date = new \DateTime();
-        $date->sub(new \DateInterval('P1D'));
+        $form = $this->createFormBuilder()
+          ->add('date', DateType::class, array(
+              'widget' => 'single_text',
+          ))
+          ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            $date = $request->get('form')['date'];
+
+            //get date which is end of date of today
+            $date = new \DateTime($date);
+            $date->add(new \DateInterval('P1D'));
+        }
+        else {
+            $date = new \DateTime();
+            $date->sub(new \DateInterval('P1D'));
+        }
 
         $expresses = $repositoryNews->getNewsByCategoryAndNewspaper(1,1,$date->format('Y-m-d H:i:s'));
         $defiMedias = $repositoryNews->getNewsByCategoryAndNewspaper(1,2,$date->format('Y-m-d H:i:s'));
         $mauriciens = $repositoryNews->getNewsByCategoryAndNewspaper(1,3,$date->format('Y-m-d H:i:s'));
 
+        //sub date to get today's date
+        $date->sub(new \DateInterval('P1D'));
+
         return $this->render('AppBundle:news:index.html.twig', array(
           'expresses' => $expresses,
           'defiMedias' => $defiMedias,
           'mauriciens' => $mauriciens,
+          'date' => $date->format('Y-m-d'),
+          'form' => $form->createView()
         ));
     }
 }
